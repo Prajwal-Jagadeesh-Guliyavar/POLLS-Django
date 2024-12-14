85139590ea3babcec05e8076ce0f007dc258d59d
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-15 03:02:18 +0530

    modified the views.py file to use the template and the render is used to pass the context and request.

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Questions
+from django.template import loader
+
 # Create your views here.
 
 def index(request):
     latest_question_list= Questions.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    #output = ", ".join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list" : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def details(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
