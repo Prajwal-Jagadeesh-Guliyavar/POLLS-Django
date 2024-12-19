f4d2d6dbc95569a543e387675ebf22646a4fbffd
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-20 00:21:55 +0530

    the DEBUG is set to TRUE since its not in the production.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ua!)skr31^3#91pf!w7hra1jl!-=og&b@dx@*(h3t$t$p@9a1p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
