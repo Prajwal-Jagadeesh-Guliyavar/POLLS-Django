0b97ce2b36020b51b3c3805a7c9960ff1c404f28
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-23 03:36:35 +0530

    added the new path to the urlpatterns list to redirect to the polls.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path,include
+
+# urlpatterns = [
+#     path('polls/',include('polls.urls')),
+#     path('admin/', admin.site.urls),
+# ]
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('polls/',include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/polls/")),  # Redirect root URL to /polls/
 ]
