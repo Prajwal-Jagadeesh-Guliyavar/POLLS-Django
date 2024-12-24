d2fe07b5ce6b5d4dbf5fea0e3c5a405ebc603a7a
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-24 16:47:54 +0530

    corrected the bug in the models.

--- polls/models.py
+++ polls/models.py
@@ -21,6 +21,8 @@ class Questions(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <=self.pub_date <=now
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
