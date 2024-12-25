3cce300ef1b75b7909f2f383aac00c6204a226fb
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-25 19:20:38 +0530

    added tests to check the Questions with the pub_date in the past are displayed with the question text while the Questions with the pub_date in the future are returned with the 404 not found.

--- polls/tests.py
+++ polls/tests.py
@@ -79,3 +79,18 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question2, question1],
         )
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        #detail view of the question with the pub_date in the future returns 404 not found
+        future_question = create_question(question_text="Fututre question", days = 5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        #detail view of the question with the pub_date in the past display's question text
+        past_question = create_question(question_text="Past question", days=-5)
+        url = reverse("polls:detail",args=(past_question.id))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
