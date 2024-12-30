eea9691cb72ed15e34f6cea68f20e61688ba9974
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2024-12-30 12:40:06 +0530

    the fieldsets in the class are changed into the list of tuples.
    the first element of the tuple is the title of the fieldset and the second element is the field in the form of a dictionary.

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Questions, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        ("Question Details", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 admin.site.register(Questions, QuestionAdmin)
 admin.site.register(Choice)
